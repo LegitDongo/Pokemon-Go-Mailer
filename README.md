e6904baeccdbfaf7544583189efa15e73e43cd5f
acsrock <ac_s_rock@yahoo.com>
2016-11-27 16:42:29 -0600

    Added IVs into Email Updated README.md Added options to config file Moved operations in code into functions, documented some.

--- README.md
+++ README.md
@@ -1,15 +1,16 @@
 # pogomailer
 A simple alert webhook module to use with the Pokemon go maps project.
 For information about the Pokemon go maps project, see https://github.com/PokemonGoMap/PokemonGo-Map
-This program works with NodeJS to send an email with essential information to inform you when there's an update to the map while "in the field"
+This program works with NodeJS to send an email with essential information to inform you when there's an update to the map while "in the field."
 
 Email format (Note the subject has the disappear time):
 
 ```
 SUBJECT: Bulbasaur 15:15:15
-Name: Bulbasaur
-ID: 1
+Name: Bulbasaur - 1
 Disappear Time: 15:15:15
+IVs: 80%
+Moveset: Vine Whip, Sledge Bomb
 Relative Address: 123 South Missouri Ave.
 Maps Url: http://maps.google.com/?g=123.123123123,123.234234234
 ```
@@ -19,11 +20,11 @@ The disappear time is in military time.
 
 # Installation
 In order to run this, you need NodeJS - https://nodejs.org/en/download/
-This program requires nodemailer, nodemailer-smtp-transport, the javascript google maps api, express, http, body-parser, request, and fs modules in nodejs.
+This program requires the modules nodemailer, nodemailer-smtp-transport, the javascript google maps api, express, body-parser, and request modules in nodejs.
 
 To install these, you can run
 `npm install modulename modulename2`
-in the command line when you are looking at the directory of the project.
+in the command line when you are looking at the directory of this project.
 
 According to https://github.com/googlemaps/google-maps-services-js you can get the javascript Google Maps API by using `npm install @google/maps` but I've never got that to work. So instead, I just use `npm install https://github.com/googlemaps/google-maps-services-js`
 
@@ -35,22 +36,28 @@ You will have to add this app to the list of webhooks for your pokemon go maps i
   An example of this (in the case of running on the same machine) would be `webhook: http://127.0.0.1:9876`
     You have to have the "http://" in front of the url.
     Remember to have the appropriate port here if it's set to something else than default.
-    
-The pogomailer.config.ini has to be in the same folder as the pogomailer.js program.
+
+You also have to change the setting `encounter` in config.ini for PokemonGo-Maps to be set to `true`
 
 Copy pogomailer.config.example.ini to pogomailer.config.ini and change the following items
 
-1. The "wantedlist" is a list of pokemon ID's (according to the global pokedex) that you want to have an email sent to you about.
   
-2. Your username/password/send to email needs to be put into the username/password/sendto parts, respectively.
+1. Your username/password/send to email needs to be put into the username/password/sendto parts, respectively.
 
-3. Put your own Google Maps API key into the APIkey section
+2. Put your own Google Maps API key into the APIkey section
 
 Some optional settings
 
-1. root - When this program is not in the root of the Pokemon Go Maps program, enter the path to the root of it.
 
-2. port - When you don't want to run this server on port 9876, enter the port you wish to run on here. Warning! There is no check to see if that port is free.
+1. wantedlist - a list of pokemon ID's (according to the global pokedex) that you want to have an email sent to you about - leave this blank to get an email about every pokemon.
+
+2. root - When this program is not in the root of the Pokemon Go Maps program, enter the path to the root of it.
+
+3. port - When you don't want to run this server on port 9876, enter the port you wish to run on here. `Warning! There is no check to see if that port is free.`
+
+4. minivpercentage - When you want to see pokemon over a certain percentage of IVs.
+
+5. ivpercentagewanted - In reference to minivpercentage. `true` = email will be sent only if that Pokemon is in the wantedlist and it's IVs are greater than or equal to the minivpercentage. `false` = email will be sent for any Pokemon that is greater than or equal to the minivpercentage. Default is false when minivpercentage is uncommented.
 
 # Run
 To start this, navigate to this project's root in command line, and use `node pogomailer.js` to start it.
@@ -62,6 +69,7 @@ Some sources say that Gmail has a maximum recepiant number of 500 per day.
 
 # Notes
 I've only tested this with Gmail accounts; they don't require a phone number like Yahoo does.
+I've only tested this with Windows 7 x64, but there is no reason why it shouldn't work for all platforms.
 
 # Me
 I'm a single developer that does this for fun. Requests will be responded to when I have free time.
--- pogomailer.js
+++ pogomailer.js
@@ -1,8 +1,7 @@
 var fs = require('fs');
-var config = fs.readFileSync('./pogomailer.config.ini', 'utf8').toString();
-config = config.split(' ').join('');
-config = config.replace(/\s*[#;].+$/gm, '');
-config = config.split(/\r?\n/);
+// Get the config file, convert to string, split into array by spaces, put back into string with no spaces, remove all parts with a "#" to the end of the line, put into array by new line
+var config = fs.readFileSync('./pogomailer.config.ini', 'utf8').toString().split(' ').join('').replace(/\s*[#;].+$/gm, '').split(/\r?\n/);
+// every place that has a ":", check to see if there is a comma in the string, if there is, put array markers around it, otherwise, leave alone
 config.forEach(function(i,o,u){
     var is = i.split(':');
     u[o] = '"' + is[0] + '":"' + is[1] + '"'
@@ -20,6 +19,45 @@ catch(e){
     console.log(e);
     process.exit(1);
 }
+
+function forceQuit(req){
+    console.log('You are missing the required config items! - ' + req + ' - Force quitting.');
+    process.exit(1);
+}
+
+//required config items
+config.APIkey || forceQuit('APIkey');
+config.username || forceQuit('username');
+config.password || forceQuit('password');
+config.sendto || forceQuit('sendto');
+
+if (config.movesets){
+    config.defense = config.movesets.includes("defense");
+    config.offense = config.movesets.includes("offense");
+}
+
+//checks to see if the number is in the Pokemon wanted list
+function inList(pokeid){
+    return config.wantedlist.indexOf(parseInt(pokeid)) >= 0
+}
+
+//returns the rounded percentage of the IV percentage
+function IVCalc(id, ia, is){
+    return Math.round((id + ia + is) / 45 * 100);
+}
+
+//returns true if meets parameters for IVs in config file
+function checkIV(pokemon){
+    if (pokemon.individual_defense && pokemon.individual_attack && pokemon.individual_stamina && typeof config.minivpercentage !== 'undefined' && config.minivpercentage > 0){
+        if ((config.ivpercentagewanted === true && inList(pokemon.pokemon_id)) || config.ivpercentagewanted === false || typeof config.ivpercentagewanted === 'undefined'){
+            if (config.minivpercentage <= IVCalc(pokemon.individual_defense, pokemon.individual_attack, pokemon.individual_stamina)){
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
 var root = config.root || './';
 if (root.charAt(root.length - 1) != '/'){
     root += '/';
@@ -33,7 +71,14 @@ var http = require('http');
 var bodyParser = require("body-parser");
 var request = require('request');
 
-var pokemons = JSON.parse(fs.readFileSync(root + 'static/data/pokemon.json', 'utf8'));
+try{
+    var pokemons = JSON.parse(fs.readFileSync(root + 'static/data/pokemon.json', 'utf8'));
+    var moves = JSON.parse(fs.readFileSync(root + 'static/data/moves.json', 'utf8'));
+}
+catch (e){
+    console.log('Error in parsing or finding config file --' + e);
+    process.exit(1);
+}
 
 var app = express();
 app.use(bodyParser.urlencoded({extended : true}));
@@ -50,16 +95,31 @@ function aZ(i){ if (i < 10){ i = "0" + i; } return i; }
 
 var latlong = 'http://maps.googleapis.com/maps/api/geocode/json?latlng=';
 var latlong2 = '&sensor=false';
-
-// var registration_ids = fs.readFileSync('./regids.txt').toString().split("\n");
+var ivpath = '/en/tools/iv-calculator';
 
 app.post('/', function(req, res) {
-    // right now the IV deteminates are sent as null
-    // console.log(req.body.message);
-    if (config.wantedlist.length == 0 || config.wantedlist.indexOf(parseInt(req.body.message.pokemon_id)) >= 0){
+    if (config.wantedlist.length === 0 || inList(req.body.message.pokemon_id) || checkIV(req.body.message)){
         gms.reverseGeocode({ latlng: [req.body.message.latitude, req.body.message.longitude] }, function(err, response){
             if (!err){
-                var name = pokemons[String(req.body.message.pokemon_id)]['name'];
+                
+                try{ var name = pokemons[String(req.body.message.pokemon_id)]['name']; }
+                catch(e){
+                    console.log('Error in getting Pokemon, try updating PokemonGo-Maps');
+                    var name = req.body.message.pokemon_id;
+                }
+                
+                try{ var move1 = moves[String(req.body.message.move_1)]['name']; }
+                catch (e){
+                    console.log('Error in getting move 1, try updating PokemonGo-Maps');
+                    var move1 = req.body.message.move_1;
+                }
+                
+                try{ var move2 = moves[String(req.body.message.move_2)]['name']; }
+                catch(e){
+                    console.log('Error in getting move 2, try updating PokemonGo-Maps');
+                    var move2 = req.body.message.move_2;
+                }
+                
                 var disappearsAt = new Date(0);
                 disappearsAt.setUTCSeconds(parseInt(req.body.message.disappear_time));
                 var transporter = nodemailer.createTransport( smtpTransport({ 
@@ -70,29 +130,31 @@ app.post('/', function(req, res) {
                     },
                     tls: { rejectUnauthorized: false }
                 } ));
-                // 'smtps://user%40gmail.com:pass@smtp.gmail.com'
+                
                 
                 var options = {
-                    from: '"Pokemon Go Maps" <' + username + '>',
+                    from: '"Pokemon Go Maps" <' + config.username + '>',
                     to: config.sendto,
                     subject: name + ' ' + aZ(disappearsAt.getHours()) + ':' + aZ(disappearsAt.getMinutes()) + ':' + aZ(disappearsAt.getSeconds()),
                     text:
-                        'Name: ' + name + '\n' +
-                        'ID: ' + req.body.message.pokemon_id + '\n' +
+                        'Name: ' + name + ' - ' + req.body.message.pokemon_id + '\n' +
                         'Disappear Time: ' + aZ(disappearsAt.getHours()) + ':' + aZ(disappearsAt.getMinutes()) + ':' + aZ(disappearsAt.getSeconds()) + '\n' +
+                        'IVs: ' + IVCalc(req.body.message.individual_defense, req.body.message.individual_attack, req.body.message.individual_stamina) + '%\n' +
+                        'Moveset: ' + move1 + ', ' + move2 + '\n' +
                         'Relative Address: ' + response.json.results[0].formatted_address + '\n' +
                         'Maps URL: ' + 'http://maps.google.com/?q=' + req.body.message.latitude + ',' + req.body.message.longitude,
-                    // html: '<b>Hello world ?</b>'
                 };
-                
                 transporter.sendMail(options, function(error, info){
-                    if (error){
-                        return console.log(error);
+                    if (!error){
+                        console.log('Sent: ' + name + '\t\t\t' + IVCalc(req.body.message.individual_defense, req.body.message.individual_attack, req.body.message.individual_stamina) + '% - ' + aZ(disappearsAt.getHours()) + ':' + aZ(disappearsAt.getMinutes()) + ':' + aZ(disappearsAt.getSeconds()));
                     }
                     else{
-                        console.log('Sent: ' + name + '\t\t' + aZ(disappearsAt.getHours()) + ':' + aZ(disappearsAt.getMinutes()) + ':' + aZ(disappearsAt.getSeconds())); //+ info.response);
+                        console.log(error);
                     }
                 });
+             }
+             else{
+                 console.log(err + ' - ' + response);
              }
          });
     }
